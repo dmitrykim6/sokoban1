ce6c9cc64a0baf28790252cdf18d58479a2ca86a
Dmitry <kido.pay@gmail.com>
2019-05-04 11:32:03 +0600

    v1.34 (add import levels)

--- src/ImportMap.java
+++ src/ImportMap.java
@@ -15,6 +15,7 @@ class ImportMap {
         3 - Box
         4 - GoalsZone
         6 - Box on GoalsZone
+        9 - Empty
 */
     }
     int[][] selLevel(int level){
@@ -34,8 +35,8 @@ class ImportMap {
             };
         } else if (level == 2) {
             desktop = new int[][]{
-                    {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-                    {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+                    {2, 2, 2, 2, 2, 2, 2, 2},
+                    {2, 0, 0, 0, 0, 0, 0, 0, 2},
                     {2, 0, 0, 0, 0, 0, 0, 0, 0, 2},
                     {2, 0, 4, 3, 0, 3, 4, 0, 0, 2},
                     {2, 0, 4, 3, 0, 3, 4, 0, 0, 2},
@@ -70,6 +71,9 @@ class ImportMap {
         String text = "";
         String[]desktopStringY;
         String[]desktopStringX;
+        String[][]desktopStringZ;
+
+        char wordToChar = ' ';
 
         fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         FileNameExtensionFilter filter = new FileNameExtensionFilter(
@@ -91,24 +95,35 @@ class ImportMap {
 
                 desktopStringX = text.split("\n");
 
+                desktop = new int[desktopStringX.length][];
+
                 for (int i = 0; i < desktopStringX.length; i++){
 
-                }
+                    desktopStringY = desktopStringX[i].split(",");
 
-                for(int a = 0; a < 10; a++){
-                    for(int b = 0; b< 10; b++){
-                        System.out.print(desktop[a][b]);
+                    desktop[i] = new int[desktopStringY.length];
+                    for(int j = 0; j < desktopStringY.length; j++){
+
+                        if (desktopStringY[j] == null || desktopStringY[j] == "9"){
+                        }else {
+
+                            wordToChar = desktopStringY[j].charAt(0);
+                            desktop[i][j] = (int)(wordToChar) - '0';
+//                            desktop[i][j] = Integer.parseInt(desktopStringY[j]);
+                        }
                     }
-                    System.out.println();
                 }
-                System.out.println();
 
 
-//                System.out.println(desktop.length);
-//                System.out.println(desktop[0].length);
+
             }catch(IOException e){
                 System.out.println("Error");
+            }catch(NumberFormatException e){
+                System.out.println("Number format error");
             }
+
+
+
         }
         return desktop;
     }
